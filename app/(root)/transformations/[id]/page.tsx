c91bf6bd1f2c26356fd622bdbbcab812688c5d2b
Shreyash Tekade <shreyashtekade2503@gmail.com>
2024-06-17 22:14:15 +0530

    Navigation bar completed

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransformationsPge = ({ params }: { params: { id: string } }) => {
+  return <div>TransformationsPge</div>;
+};
+
+export default TransformationsPge;
